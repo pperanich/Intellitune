117fc4af51b84d2c1fdd1702c6926bb7a417d839
pperanich <pperanich@gmail.com>
2018-10-11 11:22:07 -0500

    initial commit

---
+++ Debug/main.d
@@ -0,0 +1,81 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/driverlib.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430fr2355.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/legacy.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdbool.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/adc.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/crc.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/cs.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/ecomp.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/framctl.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/gpio.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/lcd_e.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/mpy32.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/pmm.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/rtc.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sac.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sfr.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sysctl.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/timer_a.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/timer_b.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/tlv.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/tri.h
+main.obj: C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/wdt_a.h
+
+../main.c: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/driverlib.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430fr2355.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/legacy.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdbool.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/adc.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/crc.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/cs.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/ecomp.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/framctl.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/gpio.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/lcd_e.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/mpy32.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/pmm.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/rtc.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sac.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sfr.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/sysctl.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/timer_a.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/timer_b.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/tlv.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/tri.h: 
+C:/Users/Preston/source/senior-design-mcu/Intellitune_msp/driverlib/MSP430FR2xx_4xx/wdt_a.h: 
