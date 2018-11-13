773a540dc99f2362d3989ff25379f9cbb25e8d5f
noahi179 <noahi179@172.18.27.79>
2018-11-13 14:53:33 -0600

    User Interface Updated. Still needs revisions.

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,66 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+LIB_SRCS += \
+/Applications/ti/MSP430Ware_3_80_05_04/iqmathlib/libraries/CCS/MPY32/5xx_6xx/IQmathLib_CCS_MPY32_5xx_6xx_CPUX_large_code_small_data.lib 
+
+C_SRCS += \
+../freq_counter.c \
+../hd44780.c \
+../intellitune.c \
+../relay.c \
+../standing_wave_sensor.c \
+../stepper_control.c \
+../user_iface.c 
+
+C_DEPS += \
+./freq_counter.d \
+./hd44780.d \
+./intellitune.d \
+./relay.d \
+./standing_wave_sensor.d \
+./stepper_control.d \
+./user_iface.d 
+
+OBJS += \
+./freq_counter.obj \
+./hd44780.obj \
+./intellitune.obj \
+./relay.obj \
+./standing_wave_sensor.obj \
+./stepper_control.obj \
+./user_iface.obj 
+
+OBJS__QUOTED += \
+"freq_counter.obj" \
+"hd44780.obj" \
+"intellitune.obj" \
+"relay.obj" \
+"standing_wave_sensor.obj" \
+"stepper_control.obj" \
+"user_iface.obj" 
+
+C_DEPS__QUOTED += \
+"freq_counter.d" \
+"hd44780.d" \
+"intellitune.d" \
+"relay.d" \
+"standing_wave_sensor.d" \
+"stepper_control.d" \
+"user_iface.d" 
+
+C_SRCS__QUOTED += \
+"../freq_counter.c" \
+"../hd44780.c" \
+"../intellitune.c" \
+"../relay.c" \
+"../standing_wave_sensor.c" \
+"../stepper_control.c" \
+"../user_iface.c" 
+
+
